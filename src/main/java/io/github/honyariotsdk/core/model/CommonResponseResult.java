6b6a33729ab4e413a974e06575cc4c4e47b9ad3d
lidesheng <3427911052@qq.com>
2022-07-26 10:52:47 +0800

    no message

--- src/main/java/io/github/honyariotsdk/core/model/CommonResponseResult.java
+++ src/main/java/io/github/honyariotsdk/core/model/CommonResponseResult.java
@@ -1,16 +1,16 @@
-package io.github.honyariotsdk.core.model;
-
-import lombok.Data;
-
-/**
- * @author lidesheng
- */
-@Data
-public class CommonResponseResult {
-
-    private Boolean success;
-    private Integer code;
-    private String message;
-    private String localizedMsg;
-    private Object data;
-}
+//package io.github.honyariotsdk.core.model;
+//
+//import lombok.Data;
+//
+///**
+// * @author lidesheng
+// */
+//@Data
+//public class CommonResponseResult {
+//
+//    private Boolean success;
+//    private Integer code;
+//    private String message;
+//    private String localizedMsg;
+//    private Object data;
+//}
--- src/main/java/io/github/honyariotsdk/device/service/impl/DeviceServiceImpl.java
+++ src/main/java/io/github/honyariotsdk/device/service/impl/DeviceServiceImpl.java
@@ -1,17 +1,18 @@
 package io.github.honyariotsdk.device.service.impl;
 
-import com.alibaba.fastjson.JSONObject;
-import io.github.honyariotsdk.core.model.CommonResponseResult;
-import io.github.honyariotsdk.core.util.HttpUtil;
+//import com.alibaba.fastjson.JSONObject;
+//import io.github.honyariotsdk.core.model.CommonResponseResult;
+//import io.github.honyariotsdk.core.util.HttpUtil;
 import io.github.honyariotsdk.device.service.DeviceService;
 
 public class DeviceServiceImpl implements DeviceService {
 
     public static void main(String[] args) {
-        JSONObject json = new JSONObject();
-        json.put("PID", "lasdkfjalksdf");
-        json.put("homeId", "1232134");
-        CommonResponseResult post = HttpUtil.post(json);
-        System.out.println(post);
+//        JSONObject json = new JSONObject();
+//        json.put("PID", "lasdkfjalksdf");
+//        json.put("homeId", "1232134");
+//        CommonResponseResult post = HttpUtil.post(json);
+//        System.out.println(post);
+        System.out.println("test");
     }
 }
